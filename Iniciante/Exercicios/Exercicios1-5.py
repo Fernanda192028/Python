cb32c9bca77e9aed4d61822216b218645b476015
Fernanda <fernandafcbpereira19071@gmail.com>
2025-06-28 21:18:33 -0300

    commit inicial

---
+++ Iniciante/Exercicios/Exercicios1-5.py
@@ -0,0 +1,39 @@
+
+# 1.Olá mundo! 
+# Mostre na tela uma mensegem "Olá mundo!" 
+# Utilize a função print 
+print("")
+
+# 2.Soma, subtração, multiplicação e divisão simples! 
+# Peça dois números inteiros ao usário
+# Mostre a soma,subtração, multiplicação e divisão deles 
+
+soma = n1 + n2 
+subtracao = n1 - n2
+multiplicacao = n1 * n2
+divisao = n1 / n2
+
+# 3.Maior de dois números
+# Leia dois números inteiros
+# mostre o maior deles
+# utilize o condional if / else
+# não esqueça de considerar se forem iguais
+
+n1 > n2
+n1 < n2
+n1 = n2 
+
+# 4.Par ou ímpar
+# Peça um número inteiro ao usúario
+# diga se ele é par ou ímpar
+# utilize o condicional if / else
+
+n1%2 == 0 (par)
+n1%2 != 0 (ímpar)
+
+
+# 5.Calculadora de IMC
+# Peça o peso e a altura de uma pessoa
+# IMC = peso / (altura ** 2)
+# Mostre o resultado do IMC
+
