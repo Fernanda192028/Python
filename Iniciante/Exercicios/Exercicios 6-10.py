470431514b43c4c873220813a9239bbc528a9711
Fernanda Cassiano <fernandafcbpereira19071@gmail.com>
2025-06-30 17:33:10 -0300

    exercicios 6-10

---
+++ Iniciante/Exercicios/Exercicios 6-10.py
@@ -0,0 +1,38 @@
+# 6. Calculo das médias 
+# Peça do usuário dois números inteiros
+#calcule a média aritmética simples 
+#calcule a média ponderada
+#considere os pesos 3 e 2
+#calcule a média geométrica 
+# utilize a biblioteca math para calcular a raiz quadrada
+# math.sqrt(x*y)
+#mostre o resultado das médias 
+import math
+
+
+# 7. Descobrindo o número de lados de um polígono
+# Peça a soma dos ângulos internos do usuário
+#calcule o número de lados do polígono
+#mostre a quantidade de lados
+
+soma_dos_angulos = (lados-2)*180
+
+#8.Número elevado ao quadrado 
+# Peça um número inteiro do usuário
+# calcule quanto vale esse numero elevado ao quadrado
+#mostre o resultado
+
+numero**2
+
+#9.Conversão de temperatura 
+#Solicite ao usuário uma temperatura em graus Celsius
+#Converta essa temperatura para graus Fahrenheit
+#Mostre a temperatura 
+
+F = (9/5*C) + 34
+
+#10. Juntando texto com números 
+#Solicite ao usuário seu nome
+#Solicite ao usuário sua idade 
+# Mostre uma mensagem no seguinte formato
+# Olá, [nome]! Você tem [idade] anos.
